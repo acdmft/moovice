eed57227dbacd6780028c6ea263941a30847f854
fusbudget <andrewsanser@gmail.com>
2022-02-15 15:11:12 +0100

    add componentDidMount in Popular.js (ex-3)

--- src/pages/Popular.js
+++ src/pages/Popular.js
@@ -1,6 +1,23 @@
 import React from "react";
 
 class Popular extends React.Component {
+  constructor() {
+    super();
+
+    this.state={
+      movies: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9f8016c7182a43a9d6ff4befd6445c3c")
+      .then((res)=> res.json())
+      .then((res)=>{
+        this.setState({movies: res.results});
+        console.log(this.state.movies)
+      })
+  }
+  
   render() {
     return (
       <h1>Popular</h1>
